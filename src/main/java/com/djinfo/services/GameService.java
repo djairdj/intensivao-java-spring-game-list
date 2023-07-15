d762b58410d8eff852dc9de41351745e412fe004
Djair Alves <djair.dj@gmail.com>
2023-07-15 16:45:44 -0300

    Definido métodos como Transactional

--- src/main/java/com/djinfo/services/GameService.java
+++ src/main/java/com/djinfo/services/GameService.java
@@ -1,9 +1,11 @@
 package com.djinfo.services;
 
+import com.djinfo.dto.GameDTO;
 import com.djinfo.dto.GameMinDTO;
 import com.djinfo.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,11 +13,13 @@ import java.util.List;
 public class GameService {
   @Autowired
   private GameRepository gameRepository;
+  @Transactional(readOnly = true)
+  public GameDTO findById(Long id){
+    var result = gameRepository.findById(id);
+    return result.map(GameDTO::new).orElse(null);
+  }
+  @Transactional(readOnly = true)
   public List<GameMinDTO> findAll(){
     return gameRepository.findAll().stream().map(GameMinDTO::new).toList();
-  }
-  public GameMinDTO find(Long id){
-    var result = gameRepository.findById(id);
-    return result.map(GameMinDTO::new).orElse(null);
   }
 }
