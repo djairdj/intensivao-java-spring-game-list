efa16f28b53576fd6512fbf7bb072870ebd383af
Djair Alves <djair.dj@gmail.com>
2023-07-15 16:09:29 -0300

    Classes Belonging, BelongingPK e GameList relacionadas

--- src/main/java/com/djinfo/entities/GameList.java
+++ src/main/java/com/djinfo/entities/GameList.java
@@ -1,6 +1,12 @@
 package com.djinfo.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_game_list")
 public class GameList {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
 
