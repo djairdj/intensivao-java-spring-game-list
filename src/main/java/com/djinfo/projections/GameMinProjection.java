a299eea563db10314f731c5d156dc6db1a8848cb
Djair Alves <djair.dj@gmail.com>
2023-07-15 17:40:05 -0300

    Native Query e sua Projection

---
+++ src/main/java/com/djinfo/projections/GameMinProjection.java
@@ -0,0 +1,10 @@
+package com.djinfo.projections;
+
+public interface GameMinProjection {
+  Long getId();
+  String getTitle();
+  Integer getYear();
+  String getImgUrl();
+  String getShortDescription();
+  Integer getPosition();
+}
